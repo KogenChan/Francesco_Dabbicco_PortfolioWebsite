c4200d854fe9681fe6617bd5a3e106f73ade0652
Evelyn <narunaga@live.it>
2025-12-19 14:12:49 +0100

    fixed a bunch of minor performance issue

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -44,6 +44,8 @@ export default function Home() {
                         className="w-full object-cover lg:w-auto lg:h-auto lg:object-contain pt-16 lg:pt-4"
                         fetchpriority="high"
                         loading="eager"
+                        width="800"
+                        height="600"
                      />
                   </figure>
 
